6f57a963584a1cb7a82f707f1a9a1efd65b69310
Juan Patrón <josejuanpatron1630@gmail.com>
2022-01-23 16:25:08 -0600

    Inicio del proyecto

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    //metodo main que ejecuta la clase
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+        int variableEntera = 20;
+        
+        System.out.println(variableEntera);
+    }
+    
+}
+
