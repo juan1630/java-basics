a188639c43e256833f973856c98602958fe0e633
Juan Patrón <josejuanpatron1630@gmail.com>
2022-01-31 18:35:06 -0600

    Concatenacion de variables strings y numericas

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -31,6 +31,46 @@ public class JavaApplication1 {
           
           System.out.println(cadenaDos);
           System.out.println("cadenaDos = " + cadenaDos);
+          
+          /*
+            nombres de variables 
+          */
+          
+          var miVariable =  1;
+          var _miVariable = 2;
+          
+          var $miVariable = 3;
+          
+          System.out.println(miVariable);
+          System.out.println(_miVariable);
+          System.out.println($miVariable);
+          
+          /*
+          No usar caracteres especiales al inicio
+          No usar acentos 
+          */
+          
+          /*
+            Concatenar variables 
+          */    
+          
+          var usuario = "Juan";
+          var saludo = "Hola";
+          var saludos  =  saludo+ " " + usuario + " ";
+          
+          System.out.println("saludos = " + saludos);
+          
+          var i = 2;
+          var j = 3;
+          
+          System.out.println(i+j); 
+        // suma de numeros enteros
+        System.out.println( i+j + saludos );
+        System.out.println( saludos+ i+j);
+        // concatena los numeros de las varibales
+        
+        System.out.println( saludos + (i+j) );
+        // los parentesis indican que la suma se hara primero
     }
     
 }
